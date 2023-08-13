20d6429732154b0ed7e3aced0a8c08f33c778201
Angel Aguilar <angeldafaq@gmail.com>
2023-08-13 18:38:18 -0500

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-const app = require ('./app');
-const {connect} = require ('./db')
+import express from 'express';
+import pg from 'pg';
+import { config } from 'dotenv';
 
-async function main(){
-    //DB connection
-    await connect();
-    //Express Application
-    app.listen(4000);
-    console.log('Server on port 4000')
-}
+config();
 
-main();
+const app = express();
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/ping', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    return res.json(result.rows[0]);
+  } catch (error) {
+    console.error('Error en la consulta:', error);
+    return res.status(500).json({ error: 'Error en la consulta' });
+  }
+});
+
+app.listen(3000);
+console.log('Server on port: 3000');
