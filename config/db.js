a9090adb3d1b9a1d425b13ca24cfed619226a677
Angel Aguilar <angeldafaq@gmail.com>
2023-08-14 16:52:39 -0500

    Last working version

--- config/db.js
+++ config/db.js
@@ -2,11 +2,8 @@ require('dotenv').config();
 
 const{ Pool } = require ('pg')
 
-//const conexion = 'postgres://proyecto_idm_user:dt0PDUcT9hoqGppKhvFUJPlhmdjf3kgb@dpg-cjc347k5kgrc7392ing0-a.oregon-postgres.render.com/proyecto_idm?ssl=true';
-
-
 const pool = new Pool({
-    //connectionString: conexion,
+
     connectionString: process.env.DATABASE_URL,
 });
 
